0af4933095a2495098b028bf226d0d84db212a83
mounikamalineni26 <mounikamalineni63@gmail.com>
2025-03-15 16:13:07 +0530

    Update josephousproblem.py

--- josephousproblem.py
+++ josephousproblem.py
@@ -2,5 +2,5 @@ n = int(input("Enter number of persons:"))
 power = 1
 while(power*2<=n):
         power *=2
-result = (n-power)*2+1
+result = (n-power)*2+1    #compute the final surviors position
 print(f"{result}th person will survive at the end")
