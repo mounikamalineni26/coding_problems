f65c0222c5ead7f3cf54857959711b7d4fd8fd82
mounikamalineni26 <mounikamalineni63@gmail.com>
2025-03-18 22:06:27 +0530

    Create towersofhanoi.py

---
+++ towersofhanoi.py
@@ -0,0 +1,7 @@
+""" TOWERS OF HANOI-
+You have three rods and N disks of different sizes, stacked in decreasing order on one rod. You need to move all the disks to another rod following these rules:
+Only one disk can be moved at a time.
+A disk can only be placed on top of a larger disk.
+You can use an auxiliary rod.
+"""
+
