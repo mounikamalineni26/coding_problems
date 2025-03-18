a5e99ba67f402354af35f08c04000bfc30c4b6be
mounikamalineni26 <mounikamalineni63@gmail.com>
2025-03-18 21:55:33 +0530

    Update josephousproblem.py

--- josephousproblem.py
+++ josephousproblem.py
@@ -1,3 +1,9 @@
+"""
+JOSEPHOUS PROBLEM---Given N people standing in a circle, 1st person has a sword and killed the 2nd person and give a sword to the 3rd personand so on...
+The challenge is to determine the position of the survivor at the end.
+"""
+
+
 n = int(input("Enter number of persons:"))
 power = 1
 while(power*2<=n):
